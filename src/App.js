5ceac1f6683fb604fd33aeb4eafb81912a7e0e94
Ali Sardari <ali-sardari@live.com>
2024-02-20 23:17:29 +0100

    - add progress - show/hide progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import SubtitlesParser from 'subtitles-parser';
 import Modal from 'react-modal';
 import {ReactComponent as MutedIcon} from './icons/muted.svg';
@@ -10,17 +10,23 @@ import {ReactComponent as SubtitleIcon} from './icons/subtitle.svg';
 import {ReactComponent as SettingsIcon} from './icons/settings.svg';
 
 const App = () => {
+    let isActiveProgress = false;
+    let mouseMoveTimer;
     const videoRef = useRef(null);
     const [subtitleEnData, setSubtitleEnData] = useState([]);
     const [subtitleFaData, setSubtitleFaData] = useState([]);
+    const [durationTime, setDurationTime] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
     const [isMuted, setIsMuted] = useState(false);
     const [volume, setVolume] = useState(1);
+    const [progress, setProgress] = useState(0);
     const [isFullscreen, setIsFullscreen] = useState(false);
+    const [isMouseMoving, setIsMouseMoving] = useState(true);
+    const [isShowHideSubtitles, setIsShowHideSubtitles] = useState(true);
 
-    //region Modal
+    //region Modal (openModal, openModal)
     const openModal = () => {
         setIsModalOpen(true);
     };
@@ -30,7 +36,7 @@ const App = () => {
     };
     //endregion
 
-    //region Handlers
+    //region Handlers (handleSubtitleEnChange, handleSubtitleFaChange, handleVideoChange)
     const handleSubtitleEnChange = (e) => {
         if (e.target.files.length > 0) {
             const reader = new FileReader();
@@ -63,16 +69,34 @@ const App = () => {
             videoRef.current.src = URL.createObjectURL(videoFile);
             videoRef.current.load();
 
+            setProgress(0);
             setIsMuted(videoRef.current.muted);
             setIsPlaying(false);
         }
     };
     //endregion
 
-    const handleTimeUpdate = () => {
+    //region Video Element (handleVideoTimeUpdate, handleVideoLoadedMetadata)
+    const handleVideoTimeUpdate = () => {
         setCurrentTime(videoRef.current.currentTime);
-    };
 
+        if (!isActiveProgress) {
+            if (durationTime !== 0) {
+                setProgress(videoRef.current.currentTime / durationTime);
+            }
+        }
+        if (videoRef.current.currentTime === durationTime) {
+            setIsPlaying(false);
+        }
+    }
+
+    function handleVideoLoadedMetadata() {
+        setDurationTime(videoRef.current.duration);
+    }
+
+    //endregion
+
+    //region Utils (timeToSeconds, formatTime)
     const timeToSeconds = (timeString) => {
         const [hh, mm, ss] = timeString.split(':').map(parseFloat);
         const seconds = hh * 3600 + mm * 60 + ss;
@@ -83,7 +107,23 @@ const App = () => {
         return seconds + milliseconds;
     };
 
-    //-------------------------------------------------------
+    const formatTime = (timeInSeconds) => {
+        const hours = Math.floor(timeInSeconds / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60);
+        const seconds = Math.floor(timeInSeconds % 60);
+
+        let formattedTime
+        if (hours > 0) {
+            formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        } else {
+            formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+        }
+
+        return formattedTime;
+    };
+    //endregion
+
+    //region Subtitle (renderSubtitleFirst, renderSubtitleSecond)
     const renderSubtitleEn = () => {
         const currentSubtitle = subtitleEnData.find(
             (subtitle) =>
@@ -103,8 +143,9 @@ const App = () => {
 
         return currentSubtitle ? currentSubtitle.text : '';
     };
+    //endregion
 
-    //-------------------------------------------------------
+    //region Controls (clickPlayPause, clickMuteToggle, toggleFullScreen, handleVolumeChange, handleProgressChange)
     const clickPlayPause = () => {
         if (!videoRef.current.currentSrc) {
             alert("Please select a video file first.");
@@ -125,7 +166,7 @@ const App = () => {
         setIsMuted(videoRef.current.muted);
     }
 
-    const toggleFullScreen =  () => {
+    const toggleFullScreen = () => {
         if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen().then(() => {
                 setIsFullscreen(true);
@@ -150,35 +191,51 @@ const App = () => {
         // setIsFullscreen(!isFullscreen);
     };
 
-
     const handleVolumeChange = (e) => {
-        const video = videoRef.current;
-
-        if (video) {
+        if (videoRef.current) {
             const newVolume = parseFloat(e.target.value);
-            video.volume = newVolume;
+            videoRef.current.volume = newVolume;
             setVolume(newVolume);
         }
     };
-    //-------------------------------------------------------
 
-    const modalStyle = {
-        content: {
-            maxWidth: '400px', // Set the maximum width of the modal content
-            margin: 'auto',    // Center the modal horizontally
-            border: '1px solid #ccc',
-            borderRadius: '5px',
-            background: 'rgba(65,65,65,0.8)',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-            color: '#f4f4f4',
-            direction: 'ltr',
-        },
-        overlay: {
-            zIndex: 1000, // Set the overlay z-index to a high value
-            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Set the overlay background color and transparency
-        },
+    const handleProgressChange = (e) => {
+        isActiveProgress = true;
+
+        if (videoRef.current) {
+            const newProgress = parseFloat(e.target.value);
+            videoRef.current.currentTime = newProgress * durationTime;
+            setProgress(newProgress);
+        }
+
+        isActiveProgress = false;
+    };
+    //endregion
+
+    const handleMouseMove = () => {
+        setIsMouseMoving(true);
+
+        // Reset the timer when the mouse moves
+        clearTimeout(mouseMoveTimer);
+
+        // Set a timeout to hide 'nex-bottom' after a delay
+        mouseMoveTimer = setTimeout(() => {
+            setIsMouseMoving(false);
+        }, 3000); // Adjust the delay (in milliseconds) as needed
     };
 
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove);
+
+        // Clean up the event listener when the component is unmounted
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    const toggleShowHideSubtitle = () => {
+        setIsShowHideSubtitles(!isShowHideSubtitles);
+    }
 
     //-------------------------------------------------------
     return (
@@ -188,24 +245,28 @@ const App = () => {
                     className='video nex-video'
                     ref={videoRef}
                     muted
-                    onTimeUpdate={handleTimeUpdate}
+                    onClick={clickPlayPause}
+                    onTimeUpdate={handleVideoTimeUpdate}
+                    onLoadedMetadata={handleVideoLoadedMetadata}
                 >
                 </video>
-                <div className='subtitles'>
+                <div className={isShowHideSubtitles ? 'subtitles' : 'hide'}>
                     <span className='subtitle-en'>{renderSubtitleEn()}</span>
                     <span className='subtitle-fa'>{renderSubtitleFa()}</span>
                 </div>
 
-                <div className="nex-bottom">
+                <div className={`nex-bottom ${isMouseMoving ? 'opacity-90' : 'opacity-0'}`}>
                     <div className="nex-progress">
                         <div className="nex-control nex-control-progress" data-index="10">
-                            <div className="nex-control-progress-inner">
-                                <div className="nex-progress-loaded" style={{width: '3.35324%'}}></div>
-                                <div className="nex-progress-played" style={{background: 'rgb(255, 173, 0)', width: '2.35273%'}}></div>
-                                <div className="nex-progress-highlight"></div>
-                                <div className="nex-progress-indicator" style={{background: 'rgb(255, 173, 0)', left: 'calc(2.35273% - 8px)'}}></div>
-                                <div className="nex-progress-tip nex-tip"></div>
-                            </div>
+                            <input
+                                type="range"
+                                min="0"
+                                max="1"
+                                step="0.0001"
+                                value={progress}
+                                onChange={handleProgressChange}
+                                className="x-progress"
+                            />
                         </div>
                         <div className="nex-control nex-control-loop" data-index="30"><span className="nex-loop-point"></span><span className="nex-loop-point"></span></div>
                     </div>
@@ -216,7 +277,6 @@ const App = () => {
                                     {isPlaying ? <StopIcon/> : <PlayIcon/>}
                                 </i>
                             </div>
-
                             <div className="nex-control nex-control-volume" data-index="20">
                                 <i
                                     aria-label="Mute"
@@ -239,10 +299,12 @@ const App = () => {
                                     />
                                 </div>
                             </div>
-                            <div className="nex-control nex-control-time nex-control-onlyText" data-index="30">01:08 / 48:19</div>
+                            <div className="nex-control nex-control-time nex-control-onlyText" data-index="30">
+                                {formatTime(currentTime)} / {formatTime(durationTime)}
+                            </div>
                         </div>
                         <div className="nex-controls-right">
-                            <div aria-label="Hide subtitle" className="nex-control nex-control-subtitle" data-balloon-pos="up" data-index="30">
+                            <div aria-label={isShowHideSubtitles ? "Hide subtitle" : "Show subtitle"} className="nex-control nex-control-subtitle" data-balloon-pos="up" data-index="30" onClick={toggleShowHideSubtitle}>
                                 <i className="nex-icon nex-icon-subtitle">
                                     <SubtitleIcon/>
                                 </i>
@@ -262,17 +324,32 @@ const App = () => {
                 </div>
             </div>
             <button className='' onClick={openModal}>Open Upload Modal</button>
-            <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={modalStyle}>
-                <h2>Upload</h2>
-                <label className='mt-2 inline-block'>
+            <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={{
+                content: {
+                    maxWidth: '400px', // Set the maximum width of the modal content
+                    margin: 'auto',    // Center the modal horizontally
+                    border: '1px solid #ccc',
+                    borderRadius: '5px',
+                    background: 'rgba(65,65,65,0.8)',
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                    color: '#f4f4f4',
+                    direction: 'ltr',
+                },
+                overlay: {
+                    zIndex: 1000, // Set the overlay z-index to a high value
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Set the overlay background color and transparency
+                },
+            }}>
+                <h2 className='text-2xl font-bold mb-4'>Upload</h2>
+                <label className='mt-4 inline-block'>
                     Choose Video:
                     <input type="file" accept=".flv, .mp4, .mkv" onChange={handleVideoChange}/>
                 </label>
-                <label className='mt-2 inline-block'>
+                <label className='mt-4 inline-block'>
                     Choose First Subtitle:
                     <input type="file" accept=".srt" onChange={handleSubtitleEnChange}/>
                 </label>
-                <label className='mt-2 inline-block'>
+                <label className='mt-4 inline-block'>
                     Choose Second Subtitle:
                     <input type="file" accept=".srt" onChange={handleSubtitleFaChange}/>
                 </label>
