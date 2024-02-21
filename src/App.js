d11db35340a21c8a096c96c65253ddc2b966c67c
Ali Sardari <ali-sardari@live.com>
2024-02-21 01:26:05 +0100

    - add setting

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = () => {
     const [isFullscreen, setIsFullscreen] = useState(false);
     const [isMouseMoving, setIsMouseMoving] = useState(true);
     const [isShowHideSubtitles, setIsShowHideSubtitles] = useState(true);
+    const [isShowHideSettings, setIsShowHideSettings] = useState(false);
 
     //region Modal (openModal, openModal)
     const openModal = () => {
@@ -176,19 +177,6 @@ const App = () => {
                 setIsFullscreen(false);
             });
         }
-
-        // if (!isFullscreen) {
-        //     document.documentElement.requestFullscreen().catch((err) => {
-        //         console.error('Error attempting to enable full screen:', err);
-        //     });
-        // } else {
-        //     document.documentElement.exitFullscreen().catch((err) => {
-        //             console.error('Error attempting to exit full screen:', err);
-        //         }
-        //     )
-        // }
-        //
-        // setIsFullscreen(!isFullscreen);
     };
 
     const handleVolumeChange = (e) => {
@@ -221,7 +209,7 @@ const App = () => {
         // Set a timeout to hide 'nex-bottom' after a delay
         mouseMoveTimer = setTimeout(() => {
             setIsMouseMoving(false);
-        }, 3000); // Adjust the delay (in milliseconds) as needed
+        }, 2000); // Adjust the delay (in milliseconds) as needed
     };
 
     useEffect(() => {
@@ -237,6 +225,14 @@ const App = () => {
         setIsShowHideSubtitles(!isShowHideSubtitles);
     }
 
+   const toggleShowHideSettings = (e) => {
+       e.stopPropagation(); // Stop the click event from propagating
+
+       console.log("aaaaaaaaaaa")
+       setIsShowHideSettings(!isShowHideSettings);
+    }
+
+
     //-------------------------------------------------------
     return (
         <div className='z-player nex-auto-size flex-col'>
@@ -245,6 +241,7 @@ const App = () => {
                     className='video nex-video'
                     ref={videoRef}
                     muted
+                    autoPlay
                     onClick={clickPlayPause}
                     onTimeUpdate={handleVideoTimeUpdate}
                     onLoadedMetadata={handleVideoLoadedMetadata}
@@ -254,7 +251,6 @@ const App = () => {
                     <span className='subtitle-en'>{renderSubtitleEn()}</span>
                     <span className='subtitle-fa'>{renderSubtitleFa()}</span>
                 </div>
-
                 <div className={`nex-bottom ${isMouseMoving ? 'opacity-90' : 'opacity-0'}`}>
                     <div className="nex-progress">
                         <div className="nex-control nex-control-progress" data-index="10">
@@ -309,7 +305,7 @@ const App = () => {
                                     <SubtitleIcon/>
                                 </i>
                             </div>
-                            <div aria-label="Show setting" className="nex-control nex-control-setting" data-balloon-pos="up" data-index="40">
+                            <div aria-label="Show setting" className="nex-control nex-control-setting" data-balloon-pos="up" data-index="40" onClick={toggleShowHideSettings}>
                                 <i className="nex-icon nex-icon-setting">
                                     <SettingsIcon/>
                                 </i>
@@ -322,39 +318,115 @@ const App = () => {
                         </div>
                     </div>
                 </div>
+                <div className={`nex-settings ${isShowHideSettings ? 'visible' : 'invisible'}`} onClick={toggleShowHideSettings}>
+                    <div className={`nex-setting-inner nex-backdrop-filter ${isShowHideSettings? 'right-0':'right-[-300px]'}`}>
+                        <div className="nex-setting-body">
+                            <div className="nex-setting nex-setting-flip" data-index="2">
+                                <div className="nex-setting-header">Flip</div>
+                                <div className="nex-setting-radio">
+                                    <div className="nex-radio-item current">
+                                        <button data-value="normal" type="button">Normal</button>
+                                    </div>
+                                    <div className="nex-radio-item">
+                                        <button data-value="horizontal" type="button">Horizontal</button>
+                                    </div>
+                                    <div className="nex-radio-item">
+                                        <button data-value="vertical" type="button">Vertical</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="nex-setting nex-setting-rotate" data-index="3">
+                                <div className="nex-setting-header">Rotate: <span className="nex-rotate-value">0°</span></div>
+                                <div className="nex-setting-radio">
+                                    <div className="nex-radio-item">
+                                        <button data-value="90" type="button">+90°</button>
+                                    </div>
+                                    <div className="nex-radio-item">
+                                        <button data-value="-90" type="button">-90°</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="nex-setting nex-setting-aspectRatio" data-index="4">
+                                <div className="nex-setting-header">Aspect ratio</div>
+                                <div className="nex-setting-radio">
+                                    <div className="nex-radio-item current">
+                                        <button data-value="default" type="button">Default</button>
+                                    </div>
+                                    <div className="nex-radio-item">
+                                        <button data-value="4:3" type="button">4:3</button>
+                                    </div>
+                                    <div className="nex-radio-item">
+                                        <button data-value="16:9" type="button">16:9</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="nex-setting nex-setting-playbackRate" data-index="5">
+                                <div className="nex-setting-header">Play speed: <span className="nex-subtitle-value">1</span>x</div>
+                                <div className="nex-setting-range"><input className="nex-subtitle-range" max="2" min="0.5" step="0.25" type="range" value="1"/></div>
+                            </div>
+                            <div className="nex-setting nex-setting-subtitleOffset" data-index="20">
+                                <div className="nex-setting-header">Subtitle offset time: <span className="nex-subtitle-value">0</span>s</div>
+                                <div className="nex-setting-range"><input className="nex-subtitle-range" id="subtitleSync" max="999" min="-999" step="0.5" type="range" value="0"/>
+                                    <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '8px'}}>
+                                        <button className="sync-btn" data-val="-5" id="minusfivesec">-5s</button>
+                                        <button className="sync-btn" data-val="-0.5" id="minushalfsec">-0.5s</button>
+                                        <button className="sync-btn" data-val="+0.5" id="plushalfsec">+0.5s</button>
+                                        <button className="sync-btn" data-val="+5" id="plusfivesec">+5s</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="nex-setting nex-setting-localVideo" data-index="30">
+                                <div className="nex-setting-header">Local Video</div>
+                                <div className="nex-setting-upload">
+                                    <div className="nex-upload-btn" style={{position: 'relative'}}>Open<input style={{position: 'absolute', width: '100%', height: '100%', left: '0px', top: '0px', opacity: 0}} type="file"/></div>
+                                    <div className="nex-upload-value"></div>
+                                </div>
+                            </div>
+                            <div className="nex-setting nex-setting-localSubtitle" data-index="40">
+                                <div className="nex-setting-header">Local Subtitle</div>
+                                <div className="nex-setting-upload">
+                                    <div className="nex-upload-btn" style={{position: 'relative'}}>Open<input style={{position: 'absolute', width: '100%', height: '100%', left: '0px', top: '0px', opacity: 0}} type="file"/></div>
+                                    <div className="nex-upload-value"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button className='' onClick={openModal}>Open Upload Modal</button>
-            <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={{
-                content: {
-                    maxWidth: '400px', // Set the maximum width of the modal content
-                    margin: 'auto',    // Center the modal horizontally
-                    border: '1px solid #ccc',
-                    borderRadius: '5px',
-                    background: 'rgba(65,65,65,0.8)',
-                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                    color: '#f4f4f4',
-                    direction: 'ltr',
-                },
-                overlay: {
-                    zIndex: 1000, // Set the overlay z-index to a high value
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Set the overlay background color and transparency
-                },
-            }}>
-                <h2 className='text-2xl font-bold mb-4'>Upload</h2>
-                <label className='mt-4 inline-block'>
-                    Choose Video:
-                    <input type="file" accept=".flv, .mp4, .mkv" onChange={handleVideoChange}/>
-                </label>
-                <label className='mt-4 inline-block'>
-                    Choose First Subtitle:
-                    <input type="file" accept=".srt" onChange={handleSubtitleEnChange}/>
-                </label>
-                <label className='mt-4 inline-block'>
-                    Choose Second Subtitle:
-                    <input type="file" accept=".srt" onChange={handleSubtitleFaChange}/>
-                </label>
-                <button className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={closeModal}>Apply</button>
-            </Modal>
+            <>
+                <button className='' onClick={openModal}>Open Upload Modal</button>
+                <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={{
+                    content: {
+                        maxWidth: '400px', // Set the maximum width of the modal content
+                        margin: 'auto',    // Center the modal horizontally
+                        border: '1px solid #ccc',
+                        borderRadius: '5px',
+                        background: 'rgba(65,65,65,0.8)',
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                        color: '#f4f4f4',
+                        direction: 'ltr',
+                    },
+                    overlay: {
+                        zIndex: 1000, // Set the overlay z-index to a high value
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Set the overlay background color and transparency
+                    },
+                }}>
+                    <h2 className='text-2xl font-bold mb-4'>Upload</h2>
+                    <label className='mt-4 inline-block'>
+                        Choose Video:
+                        <input type="file" accept=".flv, .mp4, .mkv" onChange={handleVideoChange}/>
+                    </label>
+                    <label className='mt-4 inline-block'>
+                        Choose First Subtitle:
+                        <input type="file" accept=".srt" onChange={handleSubtitleEnChange}/>
+                    </label>
+                    <label className='mt-4 inline-block'>
+                        Choose Second Subtitle:
+                        <input type="file" accept=".srt" onChange={handleSubtitleFaChange}/>
+                    </label>
+                    <button className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={closeModal}>Apply</button>
+                </Modal>
+            </>
         </div>
     );
 };
